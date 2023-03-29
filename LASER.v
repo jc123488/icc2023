99557e31df63fb158c98b16de891e19fd6cc9342
jason <jasonchou0808@gmail.com>
2023-03-29 09:27:23 +0800

    modify left right

--- LASER.v
+++ LASER.v
@@ -13,6 +13,15 @@ reg [3:0] X_in [0:39];
 reg [3:0] Y_in [0:39];
 integer i;
 
+reg [5:0] left_down_cnt, left_up_cnt, right_down_cnt, right_up_cnt;
+
+wire is_left_down, is_left_up, is_right_down, is_right_up;
+
+assign is_left_up = (X < 4'd8 && Y < 4'd8) ? 1'd1 : 1'd0;
+assign is_left_down = (X < 4'd8 && Y > 4'd7) ? 1'd1 : 1'd0;
+assign is_right_up = (X > 4'd7 && Y < 4'd8) ? 1'd1 : 1'd0;
+assign is_right_down = (X > 4'd7 && Y > 4'd7) ? 1'd1 : 1'd0;
+
 reg [1:0] state_cs, state_ns;
 parameter IDLE = 2'd0, 
           INPUT = 2'd1,
@@ -58,6 +67,29 @@ always @(posedge CLK) begin
     else if(state_cs == INPUT)begin
         X_in[cnt_40] <= X;
         Y_in[cnt_40] <= Y;
+    end
+end
+
+always @(posedge CLK or posedge RST) begin
+    if(RST)begin
+        left_down_cnt <= 6'd0;
+        left_up_cnt <= 6'd0;
+        right_down_cnt <= 6'd0;
+        right_up_cnt <= 6'd0;
+    end
+    else if(state_cs == INPUT)begin
+        if(is_left_up)begin
+            left_up_cnt <= left_up_cnt + 6'd1;
+        end
+        else if(is_left_down)begin
+            left_down_cnt <= left_down_cnt + 6'd1;
+        end
+        else if(is_right_up)begin
+            right_up_cnt <= right_up_cnt + 6'd1;
+        end
+        else begin
+            right_down_cnt <= right_down_cnt + 6'd1;
+        end
     end
 end
 
